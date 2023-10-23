93f46a5a2f7e05ed0f2b599310e8ca39e867ab33
Seraph★776 <seraph776@gmail.com>
2023-10-23 07:50:55 -0400

    Create main.py

---
+++ question_generator/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+created:10-22-2023 21:38:58 
+author:seraph★776
+email:seraph776@gmail.com
+project:Get Tarot Questions & Spread
+"""
+
+import app_func
+
+if __name__ == '__main__':
+    question_and_spread = app_func.get_results()
+    print(question_and_spread)
+    
